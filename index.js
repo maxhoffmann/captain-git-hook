4d8f331f6effffe54b03a4a2f526f3b5dbeae0c5
Maximilian Hoffmann <account@maximilianhoffmann.com>
2015-03-13 18:03:02 +0100

    fix hooks

--- index.js
+++ index.js
@@ -3,24 +3,12 @@ var fs = require('fs');
 var template = fs.readFileSync('./hook-template', 'utf-8');
 
 module.exports = function() {
-  if (process.env.npm_package_scripts_pre_commit) {
-    installHook('pre-commit');
-  }
-  if (process.env.npm_package_scripts_post_commit) {
-    installHook('post-commit');
-  }
-  if (process.env.npm_package_scripts_pre_push) {
-    installHook('pre-push');
-  }
-  if (process.env.npm_package_scripts_post_checkout) {
-    installHook('post-checkout');
-  }
-  if (process.env.npm_package_scripts_pre_receive) {
-    installHook('pre-receive');
-  }
-  if (process.env.npm_package_scripts_post_receive) {
-    installHook('post-receive');
-  }
+  installHook('pre-commit');
+  installHook('post-commit');
+  installHook('pre-push');
+  installHook('post-checkout');
+  installHook('pre-receive');
+  installHook('post-receive');
 };
 
 function installHook(hook) {
