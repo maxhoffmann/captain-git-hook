f83b0f4ef368b0f80b1874bb5a06ac4a2a0f5e34
Nicolas Deveaud <nicolas.deveaud@francetv.fr>
2016-01-11 18:29:33 +0100

    Use spawn to run npm scripts

--- src/hook-template.js
+++ src/hook-template.js
@@ -2,7 +2,7 @@
 
 "use strict";
 var path = require('path');
-var npm = require('npm');
+var spawn = require('child_process').spawn;
 
 var hookName = path.basename(__filename);
 var config = require(path.resolve(process.cwd(), 'package'));
@@ -11,18 +11,11 @@ if (!config.scripts[hookName]) {
     process.exit(0);
 }
 
-var npmConfig = {
-    loglevel: 'silent',
-    progress: false
-};
 
-npm.load(npmConfig, function() {
-    npm.run(hookName, function(error) {
-        if (error) {
-            process.exit(1);
-        }
+var npmrun = spawn('npm', ['-s', 'run', hookName], {
+    stdio: 'inherit'
+});
 
-        console.log('\nHook pass with success !!\n');
-        process.exit(0);
-    });
+npmrun.on('close', function(code) {
+    process.exit(code);
 });
--- src/install.js
+++ src/install.js
@@ -2,14 +2,8 @@
 
 var fs = require('fs');
 var path = require('path');
+
 var template = fs.readFileSync(__dirname + '/hook-template.js', 'utf-8');
-
-var projectDir = getProjectDir();
-if (process.env.NODE_ENV === 'testing') {
-    projectDir = path.join(__dirname, '../test');
-}
-
-var hookDir = path.resolve(projectDir, '.git/hooks/');
 
 var hooks = [
     'pre-commit',
@@ -18,11 +12,45 @@ var hooks = [
     'pre-push'
 ];
 
-hooks.forEach(function(hook) {
-    installHook(hook);
-});
+var projectDir = getProjectDir();
 
-function installHook(hook) {
+forEachHook(projectDir, hooks, installHook);
+
+function getProjectDir() {
+    if (process.env.NODE_ENV === 'testing') {
+        return path.join(__dirname, '../test');
+    }
+
+    var from = __dirname;
+    var to = '..';
+
+    try {
+        fs.accessSync(path.resolve(from, path.join(to, '.git')));
+    }
+    catch(error) {
+        to = '../../..';
+    }
+
+    return path.resolve(from, to);
+}
+
+function forEachHook(projectDir, hooks, action) {
+    var gitDir = path.resolve(projectDir, '.git/');
+
+    try {
+        fs.accessSync(gitDir);
+    }
+    catch(error) {
+        console.error('No git repo found, or missing permissions', projectDir);
+        return;
+    }
+
+    hooks.forEach(action.bind(null, gitDir));
+}
+
+function installHook(gitDir, hook) {
+    var hookDir = path.resolve(gitDir, 'hooks/');
+
     try {
         fs.writeFileSync(path.join(hookDir, hook), template, {
             mode: '755'
@@ -31,15 +59,4 @@ function installHook(hook) {
     } catch (error) {
         console.error('installing ' + hook + ' failed: ', error);
     }
-}
-
-function getProjectDir() {
-    var from = __dirname;
-    var to = '..';
-
-    if (!fs.existsSync('.git')) {
-        to = '../../..';
-    }
-
-    return path.resolve(from, to);
 }
