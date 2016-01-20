660f45f9020e9fe467490ae8794bd6a62b58d508
Maximilian Hoffmann <account@maximilianhoffmann.com>
2016-01-20 12:55:18 +0000

    update indentation

--- test/index.js
+++ test/index.js
@@ -5,21 +5,21 @@ var path = require('path');
 var test = require('tape');
 
 test('creates hooks', function(is) {
-    is.plan(2);
+  is.plan(2);
 
-    fs.exists(__dirname + '/.git', function(exists) {
-        if (!exists) {
-            fs.mkdirSync(__dirname + '/.git');
-            fs.mkdirSync(__dirname + '/.git/hooks');
-        }
-    });
+  fs.exists(__dirname + '/.git', function(exists) {
+    if (!exists) {
+      fs.mkdirSync(__dirname + '/.git');
+      fs.mkdirSync(__dirname + '/.git/hooks');
+    }
+  });
 
-    exec('node ' + __dirname + '/../src/install', function(error, stdout, stderr) {
-        var hooks = fs.readdirSync(__dirname + '/.git/hooks');
-        var stat = fs.statSync(path.join(__dirname, '/.git/hooks', hooks[0]));
+  exec('node ' + __dirname + '/../src/install', function(error, stdout, stderr) {
+    var hooks = fs.readdirSync(__dirname + '/.git/hooks');
+    var stat = fs.statSync(path.join(__dirname, '/.git/hooks', hooks[0]));
 
-        is.equal(hooks.length, 4);
-        is.equal(stat.mode, 33261); // 755
-    });
+    is.equal(hooks.length, 4);
+    is.equal(stat.mode, 33261); // 755
+  });
 
 });
